7e3a8ec7f30cc6e93a0c94bf2a8440d6dd2791df
Tamper Claire <tamperm1@t31300-lr017.org.aalto.fi>
2019-04-26 14:44:14 +0300

    gender guessing service

---
+++ src/genderIdentifier.py
@@ -0,0 +1,112 @@
+'''
+Created on 26 Apr 2019
+
+@author: tamperm1
+'''
+
+import re
+from collections import Counter
+from sparqlQueries import SparqlQueries
+
+class GenderIdentifier(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, name="", threshold=0.8):
+        '''
+        Constructor
+        '''
+        print("Params", name, threshold)
+        self._probability_threshold = float(threshold)
+        self.females = dict()
+        self.males = dict()
+        self.other = dict()
+        self.archive = set()
+                
+        self._name = name
+        if len(name) > 0:
+            self._gender = self.identify_gender(name)
+        else:
+            self.gender = None        
+    # getters
+        
+    def get_name(self):
+        return self._name
+    
+    def get_gender(self):
+        return self._gender
+    
+    def get_probability_threshold(self):
+        return self._probability_threshold
+    
+    # setters
+    
+    def set_name(self, name):
+        self._name = name
+    
+    def set_gender(self, gender):
+        self._gender = gender
+    
+    def set_probability_threshold(self, threshold):
+        self._probability_threshold = threshold
+        
+    def identify_gender(self, name):
+        sparql = SparqlQueries()
+        females, males, familyname = sparql.get_name_data(name)
+        
+        self.females = females
+        self.males = males
+        self.other = familyname
+        
+        return self.approx_gender(name)
+        
+    def do_split(self, txt):
+        #delimiters = " ", "...", "(", ")", "/", ",", "."
+        delimiters = [" ", "...", "(", ")", "/", ",", "."]
+        regexPattern = '|'.join(map(re.escape, delimiters))
+        return re.split(regexPattern, txt)
+    
+    '''
+    @author: ptleskinen
+    '''
+    def approx_gender(self, txt):
+        
+        print("Input", txt)
+        
+        if len(txt)<2: return None
+        if re.match(r'^[^A-ZÖÄÅ]+$',txt): return None
+        if re.match(r'.+?(poika|veli|kuningas|herttua|ruhtinas|isä|keisari|metropoliitta|piispa|prinssi)$', txt): return "Male"
+        if re.match(r'.+?(tytär|prinsessa|herttuatar|kuningatar|äiti|keisarinna|sisko|papitar)$', txt): return "Female"
+                
+        probs = [0.5,0.5]
+        for t in self.do_split(txt):
+            if re.match(r'[a-zöäå]+$',t): continue
+            
+            pM = self.males[t] if t in self.males else (10 if re.match(r'.+?poika$', t) else 1)
+            pF = self.females[t] if t in self.females else (10 if re.match(r'.+?tytär$', t) else 1)
+            
+            if pM==1 and pF==1 and re.match(r'.+?a$', t):
+                self.archive.add(t)
+            if pM+pF>0:
+                probs[0] *= pM/(pM+pF)
+                probs[1] *= pF/(pM+pF)
+            
+        
+        tot = probs[0]+probs[1]
+        if tot==0:
+            print("Unambiguous {}: {}/{}".format(txt, probs[0], probs[1]))
+            return None
+        
+        probs[0] /= tot
+        probs[1] /= tot
+        
+        if probs[0]>self._probability_threshold:
+            return "Male"
+        elif probs[1]>self._probability_threshold:
+            return "Female"
+        else:
+            print("Unambiguous {}: {}/{}".format(txt, probs[0], probs[1]))
+        
+        return None
---
+++ src/sparqlQueries.py
@@ -0,0 +1,101 @@
+'''
+Created on 26 Apr 2019
+
+@author: tamperm1
+'''
+from SPARQLWrapper import SPARQLWrapper, JSON, BASIC
+import logging
+import re
+
+class SparqlQueries(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    def query_names(self, name):
+        endpoint = "http://ldf.fi/henkilonimisto/sparql"
+        query = """
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT ?label ?gender ?count ?type WHERE {
+          VALUES ?ngram {
+            "$name"
+          }
+          BIND(STRLANG(?ngram,"fi") AS ?label)
+          ?name a <http://ldf.fi/schema/henkilonimisto/Name> .
+          ?name skos:prefLabel ?label .
+          ?name <http://ldf.fi/schema/henkilonimisto/usedAs> ?used .
+          ?used <http://ldf.fi/schema/henkilonimisto/count> ?count .
+          ?used a ?typed .
+          ?typed skos:prefLabel ?type .
+          OPTIONAL {?used <http://ldf.fi/schema/henkilonimisto/gender> ?gender .
+              #BIND(REPLACE(?genderUri, "http:\/\/schema.org\/", "", "i") AS ?gender)
+          }
+          FILTER(lang(?type) = 'fi')
+        } 
+        """
+        query = query.replace('$name', name)
+        
+        print("name=", name)
+        print("endpoint= %s", endpoint)
+        print("query= %s", query)
+
+        sparql = SPARQLWrapper(endpoint)
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        
+        return results
+    
+    def do_split(self, txt):
+        delimiters = [" ", "...", "(", ")", "/", ",", "."]
+        regexPattern = '|'.join(map(re.escape, delimiters))
+        print(regexPattern)
+        return re.split(regexPattern, txt)
+        #return filter(None, re.split("[, /!?:()]+", txt))
+    
+    def get_name_data(self, person_name):
+        males = dict()
+        females = dict()
+        other = dict()
+        names = self.do_split(person_name)
+        for name in names:
+            results = self.query_names(name)
+            
+            if len(results["results"]["bindings"]) > 0:
+                for result in results["results"]["bindings"]:
+                    label = str(result["label"]["value"])
+                    if 'gender' in result:
+                        gender = str(result["gender"]["value"]).replace("http://schema.org/", "")
+                    else:
+                        gender = ""
+                    count = int(result["count"]["value"])
+                    type = str(result["type"]["value"])
+                    
+                    print("gender:", gender)
+                    print("count:", count)
+                    print("label:", label)
+                    print("type:", type)
+                    
+                    if type == "Etunimen käyttö" and len(gender)>0:
+                        if gender == "Female":
+                            females[name] = count
+                        else:
+                            males[name] = count
+                    elif type == "Sukunimen käyttö" and len(gender) == 0:
+                        if name == names[-1]:
+                            other[name] = count
+                    else:
+                        print("Unable to identify name:", name)
+                    
+
+                        
+        return females, males, other
+    
